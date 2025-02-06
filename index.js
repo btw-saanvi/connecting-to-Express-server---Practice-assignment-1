408cb7f43857841ff1637e1d0f3e9f77bd8f4447
btw-saanvi <Saanvigarg2020@gmail.com>
2025-02-06 10:58:05 +0530

    completed the assignment

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const port = 3010;
+
+const mongoURI = process.env.MONGO_URI;
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to database');
+})
+.catch((error) => {
+  console.error('Error connecting to database:', error.message);
+});
 
 app.use(express.static('static'));
 
